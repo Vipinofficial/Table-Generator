51635e5b77cb100b669d6f198e5fd48b1f85b8ac
Vipin Kumar Soni <70593446+Vipinofficial@users.noreply.github.com>
2022-08-27 03:18:44 +0530

    27082022

---
+++ Table Generator of kids.py
@@ -0,0 +1,24 @@
+
+print ("Table Generator") 
+n= int(input("Enter a Number : "))
+a=n*1
+print("Here is your table of :" , n)
+print(n,"x 1 =", a)
+b=n*2
+print(n,"x 2 =", b)
+c=n*3
+print(n,"x 3 =", c)
+d=n*4
+print(n,"x 4 =", d)
+e=n*5
+print(n,"x 5 =", e)
+f=n*6
+print(n,"x 6 =", f)
+g=n*7
+print(n,"x 7 =", g)
+h=n*8
+print(n,"x 8 =", h)
+i=n*9
+print(n,"x 9 =", i)
+j=n*10
+print(n,"x 10 =",j)
